bc642327c6012ce40f7a9c8b2dc3e59553a807aa
iryna <irakirvas78@gmail.com>
2026-01-16 17:57:28 +0200

    Implement py-docker-weather-api

--- app/main.py
+++ app/main.py
@@ -21,7 +21,10 @@ def get_weather() -> None:
         res.raise_for_status()
         data = res.json()
         print(
-            f"{data['location']['name']}/{data['location']['country']} {data['location']['localtime']} Weather: {data['current']['temp_c']} Celsius, {data['current']['condition']['text']}"
+            f"{data['location']['name']}/{data['location']['country']}"
+            f" {data['location']['localtime']}"
+            f" Weather: {data['current']['temp_c']}"
+            f" Celsius, {data['current']['condition']['text']}"
         )
     except Exception as e:
         print(e)
